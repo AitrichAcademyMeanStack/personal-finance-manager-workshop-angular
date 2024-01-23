6cbddc365725b5903e31b2d178038de36852b85f
divyasushinanay <divyasushin.aitrich@gmail.com>
2024-01-23 16:57:37 +0530

    sidebar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,11 @@
+import { SidebarComponent } from './../shared/sidebar/sidebar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { IncomeComponent } from './income/income.component';
 import { SharedModule } from '../shared/shared.module';
+
 
 
 
@@ -16,6 +18,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule
+  
   ]
 })
 export class HomeModule { }
